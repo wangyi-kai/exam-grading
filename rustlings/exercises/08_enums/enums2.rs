51474af11398e8d5ba59be2f7ee2d891600886d7
wangyikai <466940603@qq.com>
2024-12-28 19:47:45 +0800

    add test

---
+++ rustlings/exercises/08_enums/enums2.rs
@@ -0,0 +1,38 @@
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+    Resize { width: u16, height: u16 }, // Struct-style variant
+    Move(Point),                        // Tuple-style variant
+    Echo(String),                       // Tuple-style variant
+    ChangeColor(u16, u16, u16),         // Tuple-style variant
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{self:?}");
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Resize {
+            width: 10,
+            height: 30,
+        },
+        Message::Move(Point { x: 10, y: 15 }),
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
---
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -0,0 +1,88 @@
+// This is a quiz for the following sections:
+// - Strings
+// - Vecs
+// - Move semantics
+// - Modules
+// - Enums
+//
+// Let's build a little machine in the form of a function. As input, we're going
+// to give a list of strings and commands. These commands determine what action
+// is going to be applied to the string. It can either be:
+// - Uppercase the string
+// - Trim the string
+// - Append "bar" to the string a specified amount of times
+//
+// The exact form of this will be:
+// - The input is going to be a Vector of 2-length tuples,
+//   the first element is the string, the second one is the command.
+// - The output element is going to be a vector of strings.
+
+enum Command {
+    Uppercase,
+    Trim,
+    Append(usize),
+}
+
+mod my_module {
+    use super::Command;
+
+    // TODO: Complete the function as described above.
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        //let result = Vec::with_capacity(input.len());
+        input.into_iter().map(|pair| {
+            transformer_one(pair)
+        }).collect()
+    }
+    pub fn transformer_one(input: (String, Command)) -> String {
+        let (s, cmd) = input;
+        let mut result = String::default();
+        match cmd {
+            Command::Uppercase => {
+                result = s.to_uppercase().to_string();
+            }
+            Command::Trim => {
+                result = s.trim().to_string();
+            }
+            Command::Append(len) => {
+                result += &s;
+                for _i in 0..len {
+                    result += "bar";
+                }
+            }
+        }
+        result
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO: What do we need to import to have `transformer` in scope?
+    use crate::my_module::transformer;
+use super::{*};
+    use super::Command;
+
+    #[test]
+    fn it_works() {
+        let input = vec![
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
+        ];
+        let output = transformer(input);
+
+        assert_eq!(
+            output,
+            [
+                "HELLO",
+                "all roads lead to rome!",
+                "foobar",
+                "barbarbarbarbarbar",
+            ]
+        );
+    }
+}
